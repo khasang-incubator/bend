c430abf2197b84470f3684f8727c42fb5b2b997a
ian.raievskyi <yanyska91@gmail.com>
2019-03-05 00:09:33 +0100

    Rest layer for users

--- src/main/java/io/khasang/bend/config/DataConfig.java
+++ src/main/java/io/khasang/bend/config/DataConfig.java
@@ -7,11 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -51,15 +46,15 @@ public class DataConfig {
 
 
     // non for prod
-    @Bean
-    public UserDetailsService userDetailsService() throws Exception {
-        // ensure the passwords are encoded properly
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("user").roles("USER").build());
-        manager.createUser(users.username("admin").password("admin").roles("USER","ADMIN").build());
-        return manager;
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() throws Exception {
+//        // ensure the passwords are encoded properly
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(users.username("user").password("user").roles("USER").build());
+//        manager.createUser(users.username("admin").password("admin").roles("USER","ADMIN").build());
+//        return manager;
+//    }
 
     public Environment getEnvironment() {
         return environment;
--- src/main/java/io/khasang/bend/model/ActivityType.java
+++ src/main/java/io/khasang/bend/model/ActivityType.java
@@ -3,14 +3,5 @@ package io.khasang.bend.model;
 public enum ActivityType {
     WAKEBOARD,
     KITESURF,
-    SURF;
-    private int experienceAmount; //
-
-    public int getExperienceAmount() {
-        return experienceAmount;
-    }
-
-    public void setExperienceAmount(int experienceAmount) {
-        this.experienceAmount = experienceAmount;
-    }
+    SURF
 }
