9eebbcadb61bc1aa5f64aca29083a851fdddc0c0
Ian Raievskyi <yanyska91@gmail.com>
2019-03-04 14:46:00 +0100

    to finish and test at home

---
+++ src/main/java/io/khasang/bend/model/UserStatus.java
@@ -0,0 +1,9 @@
+package io.khasang.bend.model;
+
+public enum UserStatus {
+    SEARCHING_FOR_PHOTOGRAFER,
+    SEARCHING_FOR_INSTRUCTOR,
+    SEARCHING_FOR_TRAINER,
+    ON_TRAINING,
+    EMPTY_STATUS
+}
