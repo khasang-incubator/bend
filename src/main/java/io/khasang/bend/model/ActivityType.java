f9ea085a223d9fc630f999d5f48fe64beb763a99
iraievskyi <38854720+iraievskyi@users.noreply.github.com>
2019-03-06 19:42:59 +0100

    Update ActivityType.java

--- src/main/java/io/khasang/bend/model/ActivityType.java
+++ src/main/java/io/khasang/bend/model/ActivityType.java
@@ -4,13 +4,4 @@ public enum ActivityType {
     WAKEBOARD,
     KITESURF,
     SURF;
-    private int experienceAmount; //
-
-    public int getExperienceAmount() {
-        return experienceAmount;
-    }
-
-    public void setExperienceAmount(int experienceAmount) {
-        this.experienceAmount = experienceAmount;
-    }
 }
