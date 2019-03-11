bb00d0e99c232a797a811f639e05fd99a07117dc
rsitdik <sitdikovroman79@gmail.com>
2019-03-11 12:54:23 +0300

    Revert "add entity Point"

--- src/main/java/io/khasang/bend/dao/impl/BasicDaoImpl.java
+++ src/main/java/io/khasang/bend/dao/impl/BasicDaoImpl.java
@@ -1,7 +1,6 @@
 package io.khasang.bend.dao.impl;
 
 import io.khasang.bend.dao.BasicDao;
-import io.khasang.bend.entity.Point;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
