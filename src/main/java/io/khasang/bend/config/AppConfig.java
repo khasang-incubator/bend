7672b627486e5edd85cdd1a7d719fad85a6bdc7b
skuznet <azon.sk@gmail.com>
2019-03-13 21:23:40 +0300

    fix problem with user add ManyToMany

--- src/main/java/io/khasang/bend/config/AppConfig.java
+++ src/main/java/io/khasang/bend/config/AppConfig.java
@@ -3,12 +3,15 @@ package io.khasang.bend.config;
 import io.khasang.bend.dao.AdminDao;
 import io.khasang.bend.dao.BarsukDao;
 import io.khasang.bend.dao.SchoolDao;
+import io.khasang.bend.dao.UserDao;
 import io.khasang.bend.dao.impl.AdminDaoImpl;
 import io.khasang.bend.dao.impl.BarsukDaoImpl;
 import io.khasang.bend.dao.impl.SchoolDaoImpl;
+import io.khasang.bend.dao.impl.UserDaoImpl;
 import io.khasang.bend.entity.Admin;
 import io.khasang.bend.entity.Barsuk;
 import io.khasang.bend.entity.School;
+import io.khasang.bend.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -30,5 +33,10 @@ public class AppConfig {
     @Bean
     public AdminDao adminDao() {
         return new AdminDaoImpl();
+    }
+
+    @Bean
+    public UserDao userDao() {
+        return new UserDaoImpl(User.class);
     }
 }
