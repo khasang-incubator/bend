26d8c18f3191383e0fbdcd5ebedc2f998f942f4f
rsitdik <sitdikovroman79@gmail.com>
2019-03-16 09:21:23 +0300

    add Point

---
+++ src/main/java/io/khasang/bend/service/PointService.java
@@ -0,0 +1,18 @@
+package io.khasang.bend.service;
+
+import io.khasang.bend.entity.Point;
+
+import java.util.List;
+
+public interface PointService {
+
+    Point add(Point point);
+
+    Point update(Point point);
+
+    Point delete(long id);
+
+    Point getById(long id);
+
+    List<Point> getAllPoints();
+}
