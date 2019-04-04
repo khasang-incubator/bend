c12b35163cd5dd6dee9a050cb9f15ef41af249b7
ian.raievskyi <yanyska91@gmail.com>
2019-04-04 18:52:22 +0200

    added Trainer entity, relation OneToOne with user, trainer dao + rest layer, TrainerControllerIntegrationTest

--- src/main/java/io/khasang/bend/entity/Trainer.java
+++ src/main/java/io/khasang/bend/entity/Trainer.java
@@ -1,4 +1,67 @@
 package io.khasang.bend.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "trainers")
 public class Trainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "trainer_id")
+    private long id;
+    @Column(name = "experience_as_trainer")
+    private int experienceAsTrainer;
+    @Column(name = "experience_description")
+    private String experience;
+    private String achievements;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getExperienceAsTrainer() {
+        return experienceAsTrainer;
+    }
+
+    public void setExperienceAsTrainer(int experienceAsTrainer) {
+        this.experienceAsTrainer = experienceAsTrainer;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
 }
