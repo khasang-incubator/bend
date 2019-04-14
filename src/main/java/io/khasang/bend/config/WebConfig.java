ccf00a98d9c73e0e976c484bdbf90dae9a7091f8
Ostap Strashevskii <strashevskyseo@gmail.com>
2019-04-14 17:12:16 +0300

    thymeleaf

---
+++ src/main/java/io/khasang/bend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package io.khasang.bend.config;
+
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+        import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/assets/static/img/",
+                        "classpath:/assets/static/css/",
+                        "classpath:/assets/static/js/");
+    }
+}
---
+++ src/main/java/io/khasang/bend/controller/RegistrationController.java
@@ -0,0 +1,13 @@
+package io.khasang.bend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+    @RequestMapping("/")
+    public String getBadgerPage() {
+        return "register";
+    }
+}
