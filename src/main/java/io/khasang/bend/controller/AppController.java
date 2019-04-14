d13797c575af79a78ee816dd620bb26fb1ac9ccf
Ostap Strashevskii <strashevskyseo@gmail.com>
2019-04-14 18:16:10 +0300

    cleaning

--- src/main/java/io/khasang/bend/controller/AppController.java
+++ src/main/java/io/khasang/bend/controller/AppController.java
@@ -60,8 +60,8 @@ public class AppController {
 
     @RequestMapping("/admin")
     public String getAdminPage(Model model) {
-        model.addAttribute("info", "Very secured gddfgdf info!");
-        return "gddfgdf";
+        model.addAttribute("info", "Very secured admin info!");
+        return "admin";
     }
 
     @RequestMapping("/user")
